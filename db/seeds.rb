31b5cfe24041807b1a9e5928f6e8d1c88de13c20
bbpucca <39225667+bbpucca@users.noreply.github.com>
2018-08-03 00:07:21 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(email: "user@example.com" password: "foobar", password_confirmation: "foobar")
+User.create(email: "user@example.com", password: "foobar", password_confirmation: "foobar")
