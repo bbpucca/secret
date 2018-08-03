64487fb8893ab0876690cc3a9d9f9468e7f3dea7
bbpucca <39225667+bbpucca@users.noreply.github.com>
2018-08-03 04:14:03 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/secret', as: 'secret' 
-
-  get '/signup',  to: 'users#new'
-
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-
-  get	 '/logout',  to: 'sessions#destroy'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get	'/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
